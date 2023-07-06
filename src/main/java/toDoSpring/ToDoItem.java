3c07bcebd06b2994f6cc454d65c7774d610f8148
ftitus <ftitus@users.noreply.github.com>
2023-07-05 21:08:53 -0700

    set up

---
+++ src/main/java/toDoSpring/ToDoItem.java
@@ -0,0 +1,18 @@
+package com.example.todoapp.todoitem;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class TodoItem {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String title;
+    private boolean completed;
+
+    // Constructors, getters, and setters
+}
