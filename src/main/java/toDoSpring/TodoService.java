3ed21d25b13e9993f31e9eee2ce257482f72ccf5
ftitus <ftitus@users.noreply.github.com>
2023-07-05 21:10:27 -0700

    services and framework import

---
+++ src/main/java/toDoSpring/TodoService.java
@@ -0,0 +1,22 @@
+package com.example.todoapp.todoitem;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class TodoService {
+    @Autowired
+    private TodoRepository todoRepository;
+
+    publicList<TodoItem> getAllTodoItems() {
+        return todoRepository.findAll();
+    }
+
+    public TodoItem createTodoItem(TodoItem todoItem) {
+        return todoRepository.save(todoItem);
+    }
+
+    // Implement other CRUD operations (update, delete, etc.) as needed
+}
